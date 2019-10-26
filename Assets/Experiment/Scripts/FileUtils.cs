96f2840a3351923f1f3275f84ed1e3b3003d6a3c
Damien Brun <makowildcat@gmail.com>
2019-10-26 22:02:28 +0200

    Read and set phrases from file

---
+++ Assets/Experiment/Scripts/FileUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+#if WINDOWS_UWP
+using Windows.Storage;
+#endif
+
+public static class FileUtils
+{
+    public static async Task<string> ReadTextFile(string path)
+    {
+#if WINDOWS_UWP
+        path = "Keycube" + path;
+
+        // For HoloLens, the root folder is 'Documents'
+        StorageFolder storageFolderDocuments = KnownFolders.DocumentsLibrary;
+
+        // For UWP, the path is written with '\' insteand of '/'
+        path = path.Replace("/", "\\");
+
+        StorageFile storageFile = await storageFolderDocuments.GetFileAsync(path);
+        return await FileIO.ReadTextAsync(storageFile);
+#else
+        StreamReader streamReader = File.OpenText(path);
+        string s = streamReader.ReadToEnd();
+        streamReader.Close();
+        return s;
+#endif
+    }
+}
--- Assets/Experiment/Scripts/GameManager.cs
+++ Assets/Experiment/Scripts/GameManager.cs
@@ -1,14 +1,25 @@
 ﻿using UnityEngine;
 using TMPro;
+using System.Collections;
+using System.Collections.Generic;
 
 public class GameManager : MonoBehaviour
 {
     public Keyboard keyboard;
     public TMP_InputField inputField;
+    public TextMeshProUGUI textPresented;
 
-    void Start()
+    private List<string> phrases;
+
+    async void Start()
     {
         keyboard.OnKeyPress += Keyboard_OnKeyPress;
+        inputField.Select();
+
+        string s = await FileUtils.ReadTextFile("Assets/Experiment/Resources/phrases2.txt"); 
+        phrases = new List<string>(s.Split('\n'));
+
+        SetPhrase();
     }
 
     void Keyboard_OnKeyPress(string s)
@@ -22,7 +33,7 @@ public class GameManager : MonoBehaviour
 
         if (s.Equals(">")) // enter
         {
-            keyboard.Hide();
+            SetPhrase();
             return;
         }
 
@@ -30,5 +41,13 @@ public class GameManager : MonoBehaviour
             s = " ";
 
         inputField.text += s;
+    }
+
+    private void SetPhrase()
+    {
+        int index = Random.Range(0, phrases.Count);
+        string phrase = phrases[index];
+        phrases.RemoveAt(index);
+        textPresented.text = phrase;
     }
 }
